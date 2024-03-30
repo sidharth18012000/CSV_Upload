c8d193e4877ab25e0ea57892f9511e5e65b72bb6
Sidharth Mahajan <138216077+sidharth18012000@users.noreply.github.com>
2024-03-30 15:38:35 +0530

    online db url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
-
+// mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+mongoose.connect(process.env.MONGO_DB_URL);
 const db = mongoose.connection;
 
 db.once('open', function(){
